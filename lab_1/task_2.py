254703a5856d40d91acd395c7b9f82d2555132cf
Adam Biegański <bieganski19@gmail.com>
2019-10-10 12:20:56 +0200

    Solution lab 1

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -1,15 +1,24 @@
-def task_2():
-    return ''
+def task_2(n):
+    for i in range(n+1):
+        for j in range(i):
+            print('*', end='')
+        print('')
+    for i in range(n+1, 0, -1):
+        for j in range(i):
+            print('*', end='')
+        print('')
 
 
-assert task_2() == '''
-*
-* *
-* * *
-* * * *
-* * * * *
-* * * *
-* * *
-* *
-*
-'''
+task_2(5)
+
+# assert task_2() == '''
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# '''
